8f5371a06c996c6700ae121fa8f38112e5b4b457
andre-mare <andre.mare@gmail.com>
2021-09-04 08:59:02 +0200

    Update REST Endpoints

--- start_process_scenario_02.sh
+++ start_process_scenario_02.sh
@@ -1,10 +1,10 @@
-curl --location --request POST 'http://localhost:8080/rest/process-definition/key/payment-process/start' --header 'Content-Type: application/json' --data-raw '{
+curl --location --request POST 'http://localhost:8080/engine-rest/process-definition/key/payment-process/start' --header 'Content-Type: application/json' --data-raw '{
      "businessKey": "business-key-123"
 }'
 
 sleep 5
 
-curl --location --request POST 'http://localhost:8080/rest/message' --header 'Content-Type: application/json' --data-raw '{
+curl --location --request POST 'http://localhost:8080/engine-rest/message' --header 'Content-Type: application/json' --data-raw '{
      "messageName": "payment-failed-message",
      "businessKey": "business-key-123"
 }'
